d6d133b39d92d5658160359e351af9d1b27cbb9d
mthem <mthem@INDLEYETHU>
2023-09-13 22:50:35 +0200

    Add project files.

---
+++ C# Variables/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { // declaring vaiables
+            string name;
+                string lastname = "Mthembu";  //declare and intitialize on the same line
+            int age;
+
+            // initializing
+            name = "Sisekelo";
+            age = 14;
+
+            Console.WriteLine("Hello " + name);
+            Console.WriteLine("You are " + age);
+
+            //change the value of the variables
+            name = "kING";
+            age = 15;
+            Console.WriteLine("He changed his name to " + name + " when he turned " + age);
+
+
+
+          Console.ReadLine();
+        }
+    }
+}
